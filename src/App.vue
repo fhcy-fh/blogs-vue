6c7429a13ff7b158f5d89d6b7a6e2f8f8b297463
fenghao <fh@fhcy.vip>
2024-02-27 13:56:21 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+</style>
