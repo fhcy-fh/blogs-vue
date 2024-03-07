8b024093a94b7e8a026385ad7c0b62b5e4b1ae64
fenghao <fh@fhcy.vip>
2024-03-07 17:58:33 +0800

    1

---
+++ src/components/footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div style="background-color: black; height: 50px;">
+
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  </script>
+  
+  <style>
+  </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import router from './router/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
